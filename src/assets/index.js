448b0c8432b65718eec7c2b515928d7c91f020b2
Grenish rai <ariestheracer@gmail.com>
2023-10-13 22:56:45 +0530

    export of images

---
+++ src/assets/index.js
@@ -0,0 +1,31 @@
+import git from "./git.svg";
+import getgitusers from "./getgitusers.svg";
+import coverImage1 from "./cover-image-1.png";
+import coverImage2 from "./cover-image-2.png";
+import coverImage3 from "./cover-image-3.png";
+import coverImage4 from "./cover-image-4.png";
+import coverImage5 from "./cover-image-5.png";
+import coverImage6 from "./cover-image-6.png";
+import coverImage7 from "./cover-image-7.png";
+import coverImage8 from "./cover-image-8.png";
+import coverImage9 from "./cover-image-9.png";
+import coverImage10 from "./cover-image-10.png";
+import coverImage11 from "./cover-image-11.png";
+import coverImage12 from "./cover-image-12.png";
+
+export {
+  git,
+  getgitusers,
+  coverImage1,
+  coverImage2,
+  coverImage3,
+  coverImage4,
+  coverImage5,
+  coverImage6,
+  coverImage7,
+  coverImage8,
+  coverImage9,
+  coverImage10,
+  coverImage11,
+  coverImage12,
+};
